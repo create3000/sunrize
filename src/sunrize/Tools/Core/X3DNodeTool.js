9ab0ba184299ea70fcd084be847c0e1fe2358b9f
Holger Seelig <holger.seelig@gmail.com>
2023-03-29 19:57:14 +0200

    Use privateSensors field.

--- src/sunrize/Tools/Core/X3DNodeTool.js
+++ src/sunrize/Tools/Core/X3DNodeTool.js
@@ -80,7 +80,7 @@ class X3DNodeTool
 
          X3DNodeTool .scenes .set (URL .href, scene)
 
-         scene .setPrivate (true)
+         scene .getPrivateSensors () .setValue (true)
          scene .beginUpdate ()
 
          this .tool = scene .createProto ("Tool")
