35e72b49e59c409800cb84a7705875f3e6660a9b
Holger Seelig <holger.seelig@gmail.com>
2023-06-20 19:31:09 +0200

    Added fields.pl.

---
+++ build/fields.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use v5.10.0;
+use utf8;
+use open qw/:std :utf8/;
+
+sub node {
+   $filename = shift;
+   chomp $filename;
+
+   $filename =~ m|([^/]+)/([^/]+)\.js$|;
+
+   $componentName = $1;
+   $typeName      = $2;
+
+   return if $componentName =~ /^Annotation$/;
+   return if $typeName =~ /^X3D/;
+
+   say "$componentName $typeName";
+   exit;
+}
+
+node $_ foreach sort `find ../x_ite/src/x_ite/Components -type f -mindepth 2`;
