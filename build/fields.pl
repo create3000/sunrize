51d6149d4695139160b7b85cd73ede4b5abf36d8
Holger Seelig <holger.seelig@gmail.com>
2023-06-21 19:18:31 +0200

    Updated fields.pl.

--- build/fields.pl
+++ build/fields.pl
@@ -23,33 +23,52 @@ sub node {
 
    @fields = $file =~ m|###\s*[SM]F\w+.*|go;
 
-   foreach $field (@fields)
+   field ($_, $source) foreach @fields
+}
+
+sub field {
+   $field  = shift;
+   $source = shift;
+
+   #say $field;
+
+   $field =~ /###\s*(\w+)\s*\[(.*?)\]\s*\*\*(\w+)\*\*\s*(.*?)(?:\s*<|$)/;
+
+   $type       = $1;
+   $accessType = $2;
+   $name       = $3;
+   $value      = $4;
+
+   return if $accessType eq "in";
+   return if $accessType eq "out";
+
+   $source =~ /"$name",\s*new\s+Fields\s*.*?\((.*?)\)/;
+
+   $codeValue = $1;
+
+   if ($type eq "SFBool")
    {
-      #say $field;
-
-      $field =~ /###\s*(\w+)\s*\[(.*?)\]\s*\*\*(\w+)\*\*\s*(.*?)(?:\s*<|$)/;
-
-      $type       = $1;
-      $accessType = $2;
-      $name       = $3;
-      $value      = $4;
-
-      $source =~ /"$name",\s*new\s+Fields\s*.*?\((.*?)\)/;
-
-      $codeValue = $1;
-
-      if ($type eq "SFBool")
-      {
-         say "$typeName.$name $value <-> $codeValue" if $value eq "TRUE"  && $codeValue ne "true";
-         say "$typeName.$name $value <-> $codeValue" if $value eq "FALSe" && $codeValue ne "";
-      }
-      elsif ($type eq "SFColor")
-      {
-         say "$typeName.$name $value <-> $codeValue" if $value eq "0 0 0"  && $codeValue ne "";
-         say "$typeName.$name $value <-> $codeValue" if $value eq "1 1 1" && $codeValue ne "1, 1, 1";
-         say "$typeName.$name $value <-> $codeValue" if $value eq "0.8 0.8 0.8" && $codeValue ne "0.8, 0.8, 0.8";
-      }
+      return if $value eq "TRUE"  && $codeValue eq "true";
+      return if $value eq "FALSE" && $codeValue eq "";
    }
+   # elsif ($type eq "SFColor")
+   # {
+   #    return if $value eq "0 0 0"  && $codeValue eq "";
+   #    return if $value eq "1 1 1" && $codeValue eq "1, 1, 1";
+   #    return if $value eq "0.8 0.8 0.8" && $codeValue eq "0.8, 0.8, 0.8";
+   # }
+   # elsif ($type eq "SFColorRGBA")
+   # {
+   #    return if $value eq "0 0 0"  && $codeValue eq "";
+   #    return if $value eq "1 1 1" && $codeValue eq "1, 1, 1";
+   #    return if $value eq "0.8 0.8 0.8" && $codeValue eq "0.8, 0.8, 0.8";
+   # }
+   else
+   {
+      return;
+   }
+
+   say "$typeName.$name $value <-> $codeValue";
 }
 
 node $_ foreach sort `find ../x_ite/src/x_ite/Components -type f -mindepth 2`;
