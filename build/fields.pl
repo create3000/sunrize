b4799ff0cfeeda885f019913ca5c9c04f736663c
Holger Seelig <holger.seelig@gmail.com>
2023-06-21 18:43:23 +0200

    Updated fields.pl.

--- build/fields.pl
+++ build/fields.pl
@@ -16,15 +16,16 @@ sub node {
    return if $typeName =~ /^X3D/;
 
    return unless $typeName =~ /^Transform$/;
-   say "$componentName $typeName";
+   #say "$componentName $typeName";
 
-   $file = `cat ../x_ite/docs/_posts/components/$componentName/$typeName.md`;
+   $file   = `cat ../x_ite/docs/_posts/components/$componentName/$typeName.md`;
+   $source = `cat $filename`;
 
    @fields = $file =~ m|###\s*[SM]F\w+.*|go;
 
    foreach $field (@fields)
    {
-      say $field;
+      #say $field;
 
       $field =~ /###\s*(\w+)\s*\[(.*?)\]\s*\*\*(\w+)\*\*\s*(.*?)(?:\s*<|$)/;
 
@@ -33,7 +34,15 @@ sub node {
       $name       = $3;
       $value      = $4;
 
-      say "$1 $3 $4";
+      $source =~ /"$name",\s*new\s+Fields\s*.*?\((.*?)\)/;
+
+      $codeValue = $1;
+
+      if ($1 eq "SFBool")
+      {
+         say "$typeName.$name $value <-> $codeValue" if $value eq "TRUE"  && $codeValue ne "true";
+         say "$typeName.$name$value  <-> $codeValue" if $value eq "FALSe" && $codeValue ne "";
+      }
    }
 
    #exit;
