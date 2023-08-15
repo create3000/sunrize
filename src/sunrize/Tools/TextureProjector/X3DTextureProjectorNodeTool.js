4eec9b19735e039257ee61c216b21813c7d4206d
Holger Seelig <holger.seelig@gmail.com>
2023-08-15 14:41:04 +0200

    Show on light and dim when not selected.

--- src/sunrize/Tools/TextureProjector/X3DTextureProjectorNodeTool.js
+++ src/sunrize/Tools/TextureProjector/X3DTextureProjectorNodeTool.js
@@ -14,11 +14,13 @@ class X3DTextureProjectorNodeTool extends X3DNodeTool
    {
       await super .initialize (__dirname, "X3DTextureProjectorNodeTool.x3d")
 
+      this .toolNode ._on        .addFieldInterest (this .tool .getValue () .getField ("on"))
       this .toolNode ._location  .addFieldInterest (this .tool .location)
       this .toolNode ._direction .addFieldInterest (this .tool .direction)
       this .toolNode ._upVector  .addFieldInterest (this .tool .upVector)
       this .toolNode ._texture   .addFieldInterest (this .tool .getValue () .getField ("texture"))
 
+      this .tool .on        = this .toolNode ._on
       this .tool .location  = this .toolNode ._location
       this .tool .direction = this .toolNode ._direction
       this .tool .upVector  = this .toolNode ._upVector
