319b7aa78e72111cbda6098ade07a85ff6e61dd1
Holger Seelig <holger.seelig@gmail.com>
2023-08-18 01:27:50 +0200

    Added fields.

--- src/Tools/Grouping/X3DTransformNodeTool.js
+++ src/Tools/Grouping/X3DTransformNodeTool.js
@@ -6,6 +6,14 @@ const
 
 class X3DTransformNodeTool extends X3DBoundedObjectTool
 {
+   async initialize ()
+   {
+      await super .initialize ()
+
+      this .tool .translationHandles = true
+      this .tool .rotationHandles    = true
+      this .tool .scaleHandles       = true
+   }
 }
 
 module .exports = X3DTransformNodeTool
