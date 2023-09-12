c4ff877c56f75581ede97ae2d11c932dd4deb0b9
Holger Seelig <holger.seelig@gmail.com>
2023-09-12 20:07:09 +0200

    Updated start script.

--- bin/sunrize.js
+++ bin/sunrize.js
@@ -4,10 +4,11 @@
 const os = require ("os")
 const path = require ("path")
 const { spawn } = require ("child_process")
+const cmd = os .platform () === "win32" ? "npm.cmd" : "npm"
 
 process .chdir (path .resolve (__dirname, ".."))
 
-const p = spawn (os .platform () === "win32" ? "npm.cmd" : "npm", ["start", "--silent", "--", ... process .argv .slice (2)])
+const p = spawn (cmd, ["start", "--silent", "--", ... process .argv .slice (2)])
 
 p .stdout .pipe (process .stdout)
 p .stderr .pipe (process .stderr)
