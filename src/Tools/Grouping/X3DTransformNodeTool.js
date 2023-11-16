4685f2a5978ea3dc20bca3739b04d3bfb9a568cb
Holger Seelig <holger.seelig@gmail.com>
2023-11-16 18:22:42 +0100

    Use Group for center.

--- src/Tools/Grouping/X3DTransformNodeTool.js
+++ src/Tools/Grouping/X3DTransformNodeTool.js
@@ -1,42 +1,43 @@
-"use strict"
+"use strict";
 
 const
    X3DChildNodeTool = require ("../Core/X3DChildNodeTool"),
-   X3D              = require ("../../X3D")
+   X3D              = require ("../../X3D");
 
 class X3DTransformNodeTool extends X3DChildNodeTool
 {
    async initialize ()
    {
-      await super .initialize (__dirname, "X3DTransformNodeTool.x3d")
+      await super .initialize (__dirname, "X3DTransformNodeTool.x3d");
 
-      this .toolNode .getBrowser () .displayEvents () .addInterest ("reshape", this)
+      this .toolNode .getBrowser () .displayEvents () .addInterest ("reshape", this);
 
-      this .tool .bboxDisplay = true
-      this .tool .bboxColor   = this .toolBBoxColor
+      // this .tool .centerDisplay = true;
+      this .tool .bboxDisplay   = true;
+      this .tool .bboxColor     = this .toolBBoxColor;
    }
 
    removeTool ()
    {
-      this .toolNode .getBrowser () .displayEvents () .removeInterest ("reshape", this)
+      this .toolNode .getBrowser () .displayEvents () .removeInterest ("reshape", this);
 
-      return super .removeTool ()
+      return super .removeTool ();
    }
 
-   static box = new X3D .Box3 ()
+   static box = new X3D .Box3 ();
 
    reshape ()
    {
       const
          bbox       = this .toolNode .getBBox (X3DTransformNodeTool .box),
          bboxSize   = bbox .size,
-         bboxCenter = bbox .center
+         bboxCenter = bbox .center;
 
       if (!this .tool .bboxSize .getValue () .equals (bboxSize))
-         this .tool .bboxSize = bboxSize
+         this .tool .bboxSize = bboxSize;
 
       if (!this .tool .bboxCenter .getValue () .equals (bboxCenter))
-         this .tool .bboxCenter = bboxCenter
+         this .tool .bboxCenter = bboxCenter;
    }
 }
 
