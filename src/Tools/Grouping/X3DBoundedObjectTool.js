3c48741741b21ce212f2976d76a37b0c517188f2
Holger Seelig <holger.seelig@gmail.com>
2023-11-17 00:13:32 +0100

    Added semicolons.

--- src/Tools/Grouping/X3DBoundedObjectTool.js
+++ src/Tools/Grouping/X3DBoundedObjectTool.js
@@ -1,43 +1,43 @@
-"use strict"
+"use strict";
 
 const
    X3DChildNodeTool = require ("../Core/X3DChildNodeTool"),
-   X3D              = require ("../../X3D")
+   X3D              = require ("../../X3D");
 
 class X3DBoundedObjectTool extends X3DChildNodeTool
 {
    async initialize ()
    {
-      await super .initialize (__dirname, "X3DBoundedObjectTool.x3d")
+      await super .initialize (__dirname, "X3DBoundedObjectTool.x3d");
 
-      this .getBrowser () .displayEvents () .addInterest ("reshape", this)
+      this .getBrowser () .displayEvents () .addInterest ("reshape", this);
 
-      this .tool .bboxDisplay = true
-      this .tool .bboxColor   = this .toolBBoxColor
+      this .tool .bboxDisplay = true;
+      this .tool .bboxColor   = this .toolBBoxColor;
    }
 
    removeTool ()
    {
-      this .getBrowser () .displayEvents () .removeInterest ("reshape", this)
+      this .getBrowser () .displayEvents () .removeInterest ("reshape", this);
 
-      return super .removeTool ()
+      return super .removeTool ();
    }
 
-   static box = new X3D .Box3 ()
+   static box = new X3D .Box3 ();
 
    reshape ()
    {
       const
-         bbox       = this .node .getBBox (X3DBoundedObjectTool .box),
+         bbox       = this .node .getBBox (X3DTransformNodeTool .box),
          bboxSize   = bbox .size,
-         bboxCenter = bbox .center
+         bboxCenter = bbox .center;
 
       if (!this .tool .bboxSize .getValue () .equals (bboxSize))
-         this .tool .bboxSize = bboxSize
+         this .tool .bboxSize = bboxSize;
 
       if (!this .tool .bboxCenter .getValue () .equals (bboxCenter))
-         this .tool .bboxCenter = bboxCenter
+         this .tool .bboxCenter = bboxCenter;
    }
 }
 
-module .exports = X3DBoundedObjectTool
+module .exports = X3DBoundedObjectTool;
