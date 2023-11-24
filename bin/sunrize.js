81a21eb2a0d32c90d9724dc85cb2c478f5e2f0d1
Holger Seelig <holger.seelig@gmail.com>
2023-11-24 12:19:22 +0100

    Added semicolons.

--- bin/sunrize.js
+++ bin/sunrize.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-"use strict"
+"use strict";
 
-const os = require ("os")
-const path = require ("path")
-const { spawn } = require ("child_process")
-const cmd = os .platform () === "win32" ? "npm.cmd" : "npm"
+const os = require ("os");
+const path = require ("path");
+const { spawn } = require ("child_process");
+const cmd = os .platform () === "win32" ? "npm.cmd" : "npm";
 
-process .chdir (path .resolve (__dirname, ".."))
+process .chdir (path .resolve (__dirname, ".."));
 
-const p = spawn (cmd, ["start", "--silent", "--", ... process .argv .slice (2)])
+const p = spawn (cmd, ["start", "--silent", "--", ... process .argv .slice (2)]);
 
-p .stdout .pipe (process .stdout)
-p .stderr .pipe (process .stderr)
+p .stdout .pipe (process .stdout);
+p .stderr .pipe (process .stderr);
