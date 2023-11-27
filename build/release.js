dc2e0a3799bdacc44aebf6f1a5c9bcd8187e2019
Holger Seelig <holger.seelig@gmail.com>
2023-11-27 21:37:21 +0100

    Updated release script.

--- build/release.js
+++ build/release.js
@@ -1,49 +1,51 @@
 #!/usr/bin/env node
 "use strict";
 
-const { sh } = require ("shell-tools");
+const { sh, system } = require ("shell-tools");
 
-if (sh`git branch --show-current` !== "development\n")
+async function main ()
 {
-	console .error ("Wrong branch, must be development, cannot release version!");
-	process .exit (1);
+	if (sh`git branch --show-current` !== "development\n")
+	{
+		console .error ("Wrong branch, must be development, cannot release version!");
+		process .exit (1);
+	}
+
+	// merge
+	await system (`git checkout main`);
+	await system (`git merge development`);
+
+	// version
+	const
+		name    = sh`node -p "require('./package.json').name"` .trim (),
+		online  = sh`npm view ${name} version` .trim ();
+
+	if (sh`npm pkg get version | sed 's/"//g'` .trim () === online)
+		await system (`npm version patch --no-git-tag-version --force`);
+
+	const version = sh`npm pkg get version | sed 's/"//g'` .trim ();
+
+	console .log (`NPM version ${online}`);
+	console .log (`New version ${version}`);
+
+	await system (`npm i x_ite@latest`);
+
+	// commit
+	await system (`git add -A`);
+	await system (`git commit -am 'Published version ${version}'`);
+	await system (`git push origin`);
+
+	// tag
+	await system (`git tag ${version}`);
+	await system (`git push origin --tags`);
+
+	// npm
+	await system (`npm publish`);
+
+	// development
+	await system (`git checkout development`);
+	await system (`git merge main`);
+	await system (`git push origin`);
 }
 
-// merge
-sh`git checkout main`;
-sh`git merge development`;
-
-// version
-const
-	name    = sh`node -p "require('./package.json').name"` .trim (),
-	online  = sh`npm view ${name} version` .trim ();
-
-if (sh`npm pkg get version | sed 's/"//g'` .trim () === online)
-	sh`npm version patch --no-git-tag-version --force`;
-
-const version = sh`npm pkg get version | sed 's/"//g'` .trim ();
-
-console .log (`NPM version ${online}`);
-console .log (`New version ${version}`);
-
-sh`npm i x_ite@latest`;
-
-// make
-sh`npm run make`;
-
-// commit
-sh`git add -A`;
-sh`git commit -am 'Published version ${version}'`;
-sh`git push origin`;
-
-// tag
-sh`git tag ${version}`;
-sh`git push origin --tags`;
-
-// npm
-sh`npm publish`;
-
-// development
-sh`git checkout development`;
-sh`git merge main`;
-sh`git push origin`;
+main ();
