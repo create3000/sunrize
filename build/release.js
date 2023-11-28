b4e725b370b8e648830d6d6d806b68bf483e9572
Holger Seelig <holger.seelig@gmail.com>
2023-11-28 13:35:41 +0100

    Use systemSync.

--- build/release.js
+++ build/release.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 "use strict";
 
-const { sh, system } = require ("shell-tools");
+const { sh, systemSync } = require ("shell-tools");
 
-async function main ()
+function main ()
 {
 	if (sh`git branch --show-current` !== "development\n")
 	{
@@ -12,8 +12,8 @@ async function main ()
 	}
 
 	// merge
-	await system (`git checkout main`);
-	await system (`git merge development`);
+	systemSync (`git checkout main`);
+	systemSync (`git merge development`);
 
 	// version
 	const
@@ -21,31 +21,31 @@ async function main ()
 		online  = sh`npm view ${name} version` .trim ();
 
 	if (sh`npm pkg get version | sed 's/"//g'` .trim () === online)
-		await system (`npm version patch --no-git-tag-version --force`);
+		systemSync (`npm version patch --no-git-tag-version --force`);
 
 	const version = sh`npm pkg get version | sed 's/"//g'` .trim ();
 
 	console .log (`NPM version ${online}`);
 	console .log (`New version ${version}`);
 
-	await system (`npm i x_ite@latest`);
+	systemSync (`npm i x_ite@latest`);
 
 	// commit
-	await system (`git add -A`);
-	await system (`git commit -am 'Published version ${version}'`);
-	await system (`git push origin`);
+	systemSync (`git add -A`);
+	systemSync (`git commit -am 'Published version ${version}'`);
+	systemSync (`git push origin`);
 
 	// tag
-	await system (`git tag ${version}`);
-	await system (`git push origin --tags`);
+	systemSync (`git tag ${version}`);
+	systemSync (`git push origin --tags`);
 
 	// npm
-	await system (`npm publish`);
+	systemSync (`npm publish`);
 
 	// development
-	await system (`git checkout development`);
-	await system (`git merge main`);
-	await system (`git push origin`);
+	systemSync (`git checkout development`);
+	systemSync (`git merge main`);
+	systemSync (`git push origin`);
 }
 
 main ();
