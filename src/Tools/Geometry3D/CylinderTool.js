623fa77b8cbd4411370065a6f27a2761a6501aac
Holger Seelig <holger.seelig@gmail.com>
2023-11-29 22:29:24 +0100

    Added CylinderTool.

--- src/Tools/Geometry3D/CylinderTool.js
+++ src/Tools/Geometry3D/CylinderTool.js
@@ -4,30 +4,31 @@ const
    X3DGeometryNodeTool = require ("../Rendering/X3DGeometryNodeTool"),
    X3D                 = require ("../../X3D");
 
-class ConeTool extends X3DGeometryNodeTool
+class CylinderTool extends X3DGeometryNodeTool
 {
    async initializeTool ()
    {
       await super .initializeTool ();
 
-      this .node ._height       .addInterest ("set_height_and_bottomRadius", this);
-      this .node ._bottomRadius .addInterest ("set_height_and_bottomRadius", this);
+      this .node ._height .addInterest ("set_height_and_radius", this);
+      this .node ._radius .addInterest ("set_height_and_radius", this);
 
-      this .node ._side                      .addInterest ("set_optionNode", this);
-      this .node ._bottom                    .addInterest ("set_optionNode", this);
-      this .getBrowser () .getConeOptions () .addInterest ("set_optionNode", this);
+      this .node ._side                          .addInterest ("set_optionNode", this);
+      this .node ._bottom                        .addInterest ("set_optionNode", this);
+      this .node ._top                           .addInterest ("set_optionNode", this);
+      this .getBrowser () .getCylinderOptions () .addInterest ("set_optionNode", this);
 
       this .tool .linesDisplay = true;
 
-      this .set_height_and_bottomRadius ();
+      this .set_height_and_radius ();
       this .set_optionNode ();
    }
 
-   set_height_and_bottomRadius ()
+   set_height_and_radius ()
    {
       const
          y = Math .abs (this .node ._height .getValue () / 2),
-         r = Math .abs (this .node ._bottomRadius .getValue ());
+         r = Math .abs (this .node ._radius .getValue ());
 
       this .tool .size = new X3D .Vector3 (r, y, r);
    }
@@ -35,7 +36,7 @@ class ConeTool extends X3DGeometryNodeTool
    set_optionNode ()
    {
       const
-         optionNode = this .getBrowser () .getConeOptions (),
+         optionNode = this .getBrowser () .getCylinderOptions (),
          dimension  = optionNode ._dimension .getValue (),
          coordIndex = [ ];
 
@@ -48,9 +49,15 @@ class ConeTool extends X3DGeometryNodeTool
          coordIndex .splice (-1, 0, coordIndex .at (-dimension - 1));
       }
 
+      if (this .node ._top .getValue ())
+      {
+         coordIndex .push (... optionNode .getTopGeometry () ._coordIndex);
+         coordIndex .splice (-1, 0, coordIndex .at (-dimension - 1));
+      }
+
       this .tool .set_linesCoordIndex = coordIndex;
       this .tool .linesCoord          = optionNode .getSideGeometry () ._coord;
    }
 }
 
-module .exports = ConeTool;
+module .exports = CylinderTool;
