fff539c79534bd35b936e62749620cc86ba49e7f
Holger Seelig <holger.seelig@gmail.com>
2023-11-30 00:06:23 +0100

    Enable viewAll for geometry nodes.

--- src/Application/Dashboard.js
+++ src/Application/Dashboard.js
@@ -1,20 +1,20 @@
-"use strict"
+"use strict";
 
 const
    $         = require ("jquery"),
    X3D       = require ("../X3D"),
    Interface = require ("./Interface"),
-   _         = require ("./GetText")
+   _         = require ("./GetText");
 
 module .exports = class Dashboard extends Interface
 {
    constructor (element)
    {
-      super ("Sunrize.Dashboard.")
+      super ("Sunrize.Dashboard.");
 
-      this .toolbar = element
+      this .toolbar = element;
 
-      this .setup ()
+      this .setup ();
    }
 
    async initialize ()
@@ -23,15 +23,15 @@ module .exports = class Dashboard extends Interface
          .addClass (["image-icon", "hand"])
          .attr ("title", _ ("Switch to browser mode."))
          .appendTo (this .toolbar)
-         .on ("click", () => this .hand ())
+         .on ("click", () => this .hand ());
 
-      this .handButton .addClass ("active")
+      this .handButton .addClass ("active");
 
       this .arrowButton = $("<span></span>")
          .addClass (["image-icon", "arrow"])
          .attr ("title", _ ("Switch to edit mode."))
          .appendTo (this .toolbar)
-         .on ("click", () => this .arrow ())
+         .on ("click", () => this .arrow ());
 
       this .playButton = $("<span></span>")
          .addClass (["material-icons"])
@@ -39,23 +39,23 @@ module .exports = class Dashboard extends Interface
          .css ({ position: "relative", left: "-1px", "font-weight": "bold" })
          .text ("play_arrow")
          .appendTo (this .toolbar)
-         .on ("click", () => this .play (!this .fileConfig .play))
+         .on ("click", () => this .play (!this .fileConfig .play));
 
-      $("<span></span>") .addClass ("separator") .appendTo (this .toolbar)
+      $("<span></span>") .addClass ("separator") .appendTo (this .toolbar);
 
       this .viewAllButton = $("<span></span>")
          .addClass (["material-symbols-outlined"])
          .attr ("title", _ ("View all objects in active layer."))
          .text ("center_focus_strong")
          .appendTo (this .toolbar)
-         .on ("click", () => this .viewAll ())
+         .on ("click", () => this .viewAll ());
 
       this .straightenButton = $("<span></span>")
          .addClass (["material-symbols-outlined", "active"])
          .attr ("title", _ ("Straighten horizon."))
          .text ("wb_twilight")
          .appendTo (this .toolbar)
-         .on ("click", () => this .straighten (!this .browser .getBrowserOption ("StraightenHorizon")))
+         .on ("click", () => this .straighten (!this .browser .getBrowserOption ("StraightenHorizon")));
    }
 
    configure ()
@@ -63,56 +63,58 @@ module .exports = class Dashboard extends Interface
       this .fileConfig .setDefaultValues ({
          pointer: "hand",
          play: false,
-      })
+      });
 
-      this [this .fileConfig .pointer] ()
-      this .play (this .fileConfig .play)
-      this .straighten (this .browser .getBrowserOption ("StraightenHorizon"))
+      this [this .fileConfig .pointer] ();
+      this .play (this .fileConfig .play);
+      this .straighten (this .browser .getBrowserOption ("StraightenHorizon"));
    }
 
    hand ()
    {
-      this .fileConfig .pointer = "hand"
+      this .fileConfig .pointer = "hand";
 
       if (this .handButton .hasClass ("active"))
-         return
+         return;
 
-      this .arrowButton .removeClass ("active")
-      this .handButton .addClass ("active")
+      this .arrowButton .removeClass ("active");
+      this .handButton .addClass ("active");
    }
 
    arrow ()
    {
-      this .fileConfig .pointer = "arrow"
+      this .fileConfig .pointer = "arrow";
 
       if (this .arrowButton .hasClass ("active"))
-         return
+         return;
 
-      this .handButton .removeClass ("active")
-      this .arrowButton .addClass ("active")
+      this .handButton .removeClass ("active");
+      this .arrowButton .addClass ("active");
    }
 
    play (value)
    {
-      this .fileConfig .play = value
+      this .fileConfig .play = value;
 
       if (value)
       {
-         this .playButton .addClass ("active")
-         this .browser .beginUpdate ()
+         this .playButton .addClass ("active");
+         this .browser .beginUpdate ();
       }
       else
       {
-         this .playButton .removeClass ("active")
-         this .browser .endUpdate ()
+         this .playButton .removeClass ("active");
+         this .browser .endUpdate ();
       }
    }
 
    viewAll ()
    {
+      const types = new Set ([X3D .X3DConstants .X3DBoundedObject, X3D .X3DConstants .X3DGeometryNode]);
+
       const
          outlineEditor = require ("./Window") .sidebar .outlineEditor,
-         selection     = outlineEditor .sceneGraph .find (".node.selected")
+         selection     = outlineEditor .sceneGraph .find (".node.selected");
 
       if (selection .length)
       {
@@ -120,20 +122,20 @@ module .exports = class Dashboard extends Interface
             layerNode     = this .browser .getActiveLayer (),
             viewpointNode = this .browser .getActiveViewpoint (),
             bbox          = new X3D .Box3 (),
-            straighten    = this .browser .getBrowserOption ("StraightenHorizon")
+            straighten    = this .browser .getBrowserOption ("StraightenHorizon");
 
          for (const element of selection)
          {
             try
             {
-               const node = outlineEditor .getNode ($(element)) .getInnerNode ()
+               const node = outlineEditor .getNode ($(element)) .getInnerNode ();
 
-               if (!node .getType () .includes (X3D .X3DConstants .X3DBoundedObject))
-                  continue
+               if (!node .getType () .some (type => types .has (type)))
+                  continue;
 
-               const modelMatrix = outlineEditor .getModelMatrix ($(element), false)
+               const modelMatrix = outlineEditor .getModelMatrix ($(element), false);
 
-               bbox .add (node .getBBox (new X3D .Box3 ()) .multRight (modelMatrix))
+               bbox .add (node .getBBox (new X3D .Box3 ()) .multRight (modelMatrix));
             }
             catch
             { }
@@ -142,22 +144,22 @@ module .exports = class Dashboard extends Interface
          if (!bbox .size .magnitude ())
             return;
 
-         viewpointNode .lookAtBBox (layerNode, bbox, 1, straighten)
+         viewpointNode .lookAtBBox (layerNode, bbox, 1, straighten);
       }
       else
       {
-         this .browser .viewAll ()
+         this .browser .viewAll ();
       }
    }
 
    straighten (value)
    {
-      this .browser .setBrowserOption ("StraightenHorizon", value)
+      this .browser .setBrowserOption ("StraightenHorizon", value);
 
       if (value)
-         this .straightenButton .addClass ("active")
+         this .straightenButton .addClass ("active");
       else
-         this .straightenButton .removeClass ("active")
+         this .straightenButton .removeClass ("active");
    }
-}
+};
 
