e8ee3e85fb6c2f1d723cd592feecf968b374368b
Holger Seelig <holger.seelig@gmail.com>
2023-11-30 10:25:07 +0100

    Only create coordIndex if needed.

--- src/Tools/Rendering/X3DGeometryNodeTool.js
+++ src/Tools/Rendering/X3DGeometryNodeTool.js
@@ -15,31 +15,36 @@ class X3DGeometryNodeTool extends X3DNodeTool
       if (type === "CUSTOM")
          return;
 
-      this .node ._rebuild .addInterest ("set_vertices", this);
+      this .node ._rebuild .addInterest ("set_geometry", this);
 
       this .tool .linesDisplay = true;
       this .tool .linesCoord   = this .getToolScene () .createNode ("Coordinate");
 
-      this .set_vertices ();
+      this .set_geometry ();
    }
 
    disposeTool ()
    {
-      this .node ._rebuild .removeInterest ("set_vertices", this);
+      this .node ._rebuild .removeInterest ("set_geometry", this);
    }
 
-   set_vertices ()
+   set_geometry ()
    {
       const
-         coordIndex = [ ],
-         points     = this .node .getVertices () .filter ((_, i) => i % 4 !== 3),
-         length     = points .length / 3;
+         points = this .node .getVertices () .filter ((_, i) => i % 4 !== 3),
+         length = points .length / 3;
 
-      for (let i = 0; i < length; i += 3)
-         coordIndex .push (i, i + 1, i + 2, i, -1);
+      if (length !== this .tool .linesCoord .point .length)
+      {
+         const coordIndex = [ ];
 
-      this .tool .set_linesCoordIndex = coordIndex;
-      this .tool .linesCoord .point   = points;
+         for (let i = 0; i < length; i += 3)
+            coordIndex .push (i, i + 1, i + 2, i, -1);
+
+         this .tool .set_linesCoordIndex = coordIndex;
+      }
+
+      this .tool .linesCoord .point = points;
    }
 }
 
