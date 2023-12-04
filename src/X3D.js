db533f8d05eb3b096ef3405cb824191c47594b8b
Holger Seelig <holger.seelig@gmail.com>
2023-12-05 00:13:44 +0100

    Removed comments.

--- src/X3D.js
+++ src/X3D.js
@@ -1,8 +1,5 @@
 "use strict";
 
-/**
- * @type {{ SFBool: SFBool, SFColor: SFColor, }}
- */
 const X3D = process .env .SUNRISE_ENVIRONMENT === "DEVELOPMENT" ? require ("../x_ite/x_ite.js") : require ("x_ite");
 
 module .exports = X3D;
