dbe775feb70a0be65c5a1b5ced6b426712a5b181
Holger Seelig <holger.seelig@gmail.com>
2023-12-16 14:01:59 +0100

    Added downloading class.

--- docs/assets/js/download.js
+++ docs/assets/js/download.js
@@ -6,9 +6,11 @@
 
       const
          response = await fetch (element .attr ("href")),
-         total    = parseInt (response .headers .get ("content-length")) || element .attr ("content-length"),
+         total    = parseInt (response .headers .get ("content-length")) || parseInt (element .attr ("content-length")),
          reader   = response .body .getReader (),
          chunks   = [ ];
+
+      element .addClass ("downloading");
 
       let received = 0;
 
@@ -20,28 +22,16 @@
             break;
 
          chunks .push (value);
+
          received += value .length;
 
          const progress = received / total;
 
-         element .attr ("style", "color: black !important");
-         element .css ("border-color", "var(--system-green)");
-         element .css ("background", `linear-gradient(90deg, var(--system-green) ${progress * 100}%, var(--download-bg) ${progress * 100}%)`);
-      }
-
-      const data = new Uint8Array (received);
-
-      let position = 0;
-
-      for (const chunk of chunks)
-      {
-         data .set (chunk, position);
-
-         position += chunk .length;
+         element .css ("background", `linear-gradient(90deg, var(--downloading-left) ${progress * 100}%, var(--downloading-right) ${progress * 100}%)`);
       }
 
       const
-         blob = new Blob ([data],  { type: "application/octet-stream" }),
+         blob = new Blob (chunks, { type: "application/octet-stream" }),
          url  = URL .createObjectURL (blob),
          link = document .createElement ("a");
 
@@ -50,8 +40,7 @@
 
       link .click ();
 
-      element .css ("color", "");
-      element .css ("border-color", "");
+      element .removeClass ("downloading");
       element .css ("background", "");
    })();
 
