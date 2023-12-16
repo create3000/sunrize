382ed1c07e6c7104e0bf001e71335eac2546bfd2
Holger Seelig <holger.seelig@gmail.com>
2023-12-16 22:18:08 +0100

    Updated main.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 "use strict";
 
 require ("update-electron-app") .updateElectronApp ();
-
-const Application = require ("./Application/Application");
-
-Application .run ();
+require ("./Application/Application") .run ();
