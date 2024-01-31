2d55323dc04413f0c4f2e96582fd2c1c0eb8383e
Holger Seelig <holger.seelig@gmail.com>
2024-01-31 11:17:40 +0100

    Automatically create blades.

--- src/Editors/Panel.js
+++ src/Editors/Panel.js
@@ -2,7 +2,8 @@ const
    $         = require ("jquery"),
    TweakPane = require ("tweakpane"),
    Interface = require ("../Application/Interface"),
-   X3D       = require ("../X3D");
+   X3D       = require ("../X3D"),
+   Editor    = require ("../Undo/Editor");
 
 module .exports = new class Panel extends Interface
 {
@@ -66,90 +67,173 @@ module .exports = new class Panel extends Interface
 
    onselection ()
    {
-      const node = this .selection .nodes .at (-1);
-
-
       // Remove all blades.
 
       for (const folder of [... this .pane .children])
          folder .dispose ();
 
-      if (!node)
+      if (this .node)
+      {
+         for (const field of this .node .getFields ())
+            field .removeFieldCallback (this);
+      }
+
+      // Get new node.
+
+      this .node = this .selection .nodes .at (-1);
+
+      if (!this .node)
       {
          this .pane .hidden = true;
          return;
       }
 
-      // Add new blades.
+      this .addBlades (this .node);
 
+      this .pane .hidden = !this .pane .children .length;
+   }
+
+   addBlades (node)
+   {
       for (const type of node .getType () .toReversed ())
       {
          switch (type)
          {
-            case X3D .X3DConstants .Shape:
-            {
-               const folder = this .pane .addFolder ({
-                  title: "Shape",
-                  expanded: this .fileConfig [`Shape.expanded`] ?? true,
-               });
-
-               const parameter = {
-                  castShadow: node ._castShadow .getValue (),
-               };
-
-               folder .addInput (parameter, "castShadow");
-
-               break;
-            }
             case X3D .X3DConstants .Transform:
             {
-               const folder = this .pane .addFolder ({
+               this .addFolder ({
                   title: node .getTypeName (),
-                  expanded: this .fileConfig [`${node .getTypeName ()}.expanded`] ?? true,
+                  node: node,
+                  fields: [
+                     "translation",
+                     "rotation",
+                     "scale",
+                     "---",
+                     "scaleOrientation",
+                     "center",
+                  ],
                });
-
-               const parameter = {
-                  translation: { x: node ._translation .x, y: node ._translation .y, z: node ._translation .z },
-                  rotation: { x: node ._rotation .x, y: node ._rotation .y, z: node ._rotation .z, w: node ._rotation .angle },
-                  scale: { x: node ._scale .x, y: node ._scale .y, z: node ._scale .z },
-               };
-
-               folder .addInput (parameter, "translation")
-                  .on ("change", ({ value }) => console .log (value .x, value .y, value .z));
-               folder .addInput (parameter, "rotation");
-               folder .addInput (parameter, "scale");
 
                break;
             }
             case X3D .X3DConstants .X3DBoundedObject:
             {
-               const folder = this .pane .addFolder ({
+               this .addFolder ({
                   title: "X3DBoundedObject",
-                  expanded: this .fileConfig [`X3DBoundedObject.expanded`] ?? false,
+                  node: node,
+                  fields: [
+                     "visible",
+                     "bboxDisplay",
+                     "bboxSize",
+                     "bboxCenter",
+                  ],
                });
 
-               const parameter = {
-                  visible: node ._visible .getValue (),
-                  bboxDisplay: node ._bboxDisplay .getValue (),
-                  bboxSize: { x: node ._bboxSize .x, y: node ._bboxSize .y, z: node ._bboxSize .z },
-                  bboxCenter: { x: node ._bboxCenter .x, y: node ._bboxCenter .y, z: node ._bboxCenter .z },
-               };
-
-               folder .addInput (parameter, "visible");
-               folder .addInput (parameter, "bboxDisplay");
-               folder .addInput (parameter, "bboxSize");
-               folder .addInput (parameter, "bboxCenter");
+               break;
+            }
+            case X3D .X3DConstants .X3DShapeNode:
+            {
+               this .addFolder ({
+                  title: "X3DShapeNode",
+                  node: node,
+                  fields: [
+                     "castShadow",
+                  ],
+               });
 
                break;
             }
          }
       }
+   }
 
-      this .pane .hidden = !this .pane .children .length;
+   addFolder ({ title, node, fields })
+   {
+      const folder = this .pane .addFolder ({
+         title: title,
+         expanded: this .fileConfig [`${title}.expanded`] ?? !this .pane .children .length,
+      });
 
       // Update expanded state of folder.
 
-      for (const folder of this .pane .children)
-         folder .on ("fold", () => this .fileConfig [`${folder .title}.expanded`] = folder .expanded)
+      folder .on ("fold", () => this .fileConfig [`${title}.expanded`] = folder .expanded)
+
+      // Add fields.
+
+      const parameter = { };
+
+      for (const name of fields)
+      {
+         if (name .match (/^-+$/))
+            folder .addSeparator ();
+         else
+            this .addInput (folder, parameter, node, node .getField (name));
+      }
+
+      return folder;
+   }
+
+   addInput (folder, parameter, node, field)
+   {
+      switch (field .getType ())
+      {
+         case X3D .X3DConstants .SFBool:
+         case X3D .X3DConstants .SFVec3d:
+         case X3D .X3DConstants .SFVec3f:
+         {
+            this .refresh (parameter, node, field);
+
+            const input = folder .addInput (parameter, field .getName ())
+               .on ("change", ({ value }) => this .onchange (node, field, value));
+
+            field .addFieldCallback (this, () =>
+            {
+               this .refresh (parameter, node, field);
+               input .refresh ();
+            });
+         }
+      }
+   }
+
+   refresh (parameter, node, field)
+   {
+      switch (field .getType ())
+      {
+         case X3D .X3DConstants .SFBool:
+         {
+            parameter [field .getName ()] = field .getValue ();
+            break;
+         }
+         case X3D .X3DConstants .SFVec3d:
+         case X3D .X3DConstants .SFVec3f:
+         {
+            parameter [field .getName ()] ??= { };
+
+            parameter [field .getName ()] .x = field .x;
+            parameter [field .getName ()] .y = field .y;
+            parameter [field .getName ()] .z = field .z;
+            break;
+         }
+      }
+   }
+
+   onchange (node, field, value)
+   {
+      const executionContext = node .getExecutionContext ();
+
+      switch (field .getType ())
+      {
+         case X3D .X3DConstants .SFBool:
+         {
+            Editor .setFieldValue (executionContext, node, field, value);
+            break;
+         }
+         case X3D .X3DConstants .SFVec3d:
+         case X3D .X3DConstants .SFVec3f:
+         {
+            Editor .setFieldValue (executionContext, node, field, new X3D .Vector3 (value .x, value .y, value .z));
+            break;
+         }
+      }
    }
 };
