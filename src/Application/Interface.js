7fe183f4fc1279adb284d32768e8177989bae313
Holger Seelig <holger.seelig@gmail.com>
2024-02-11 14:04:20 +0100

    Added semicolons.

--- src/Application/Interface.js
+++ src/Application/Interface.js
@@ -30,89 +30,89 @@ Object .assign ($,
 
 module .exports = class Interface
 {
-   static #interfaces   = new Set ()
-   static #initialScene = X3D .getBrowser ("#browser") .currentScene
+   static #interfaces   = new Set ();
+   static #initialScene = X3D .getBrowser ("#browser") .currentScene;
 
    constructor (namespace)
    {
-      Interface .#interfaces .add (this)
+      Interface .#interfaces .add (this);
 
-      this .namespace    = namespace
-      this .globalConfig = this .#createGlobalConfig ()
-      this .fileConfig   = this .#createFileConfig ("")
+      this .namespace    = namespace;
+      this .globalConfig = this .#createGlobalConfig ();
+      this .fileConfig   = this .#createFileConfig ("");
 
-      this .browser .addBrowserCallback (this, X3D .X3DConstants .INITIALIZED_EVENT, this .browserInitialized .bind (this))
-      CSS .colorScheme .addEventListener ("change", event => this .colorScheme (!! event .matches))
+      this .browser .addBrowserCallback (this, X3D .X3DConstants .INITIALIZED_EVENT, this .browserInitialized .bind (this));
+      CSS .colorScheme .addEventListener ("change", event => this .colorScheme (!! event .matches));
    }
 
    setup ()
    {
-      this .initialize ()
+      this .initialize ();
 
       // Configure
 
-      this .browserInitialized (X3D .X3DConstants .INITIALIZED_EVENT)
+      this .browserInitialized (X3D .X3DConstants .INITIALIZED_EVENT);
    }
 
    initialize () { }
 
-   static #idMap     = new WeakMap ()
-   static #idCounter = 0
+   static #idMap     = new WeakMap ();
+   static #idCounter = 0;
 
    static getId (object)
    {
-      const id = Interface .#idMap .get (object)
+      const id = Interface .#idMap .get (object);
 
       if (id !== undefined)
-         return id
+         return id;
 
-      Interface .#idMap .set (object, ++ Interface .#idCounter)
+      Interface .#idMap .set (object, ++ Interface .#idCounter);
 
-      return Interface .#idCounter
+      return Interface .#idCounter;
    }
 
    get id ()
    {
-      return Interface .getId (this)
+      return Interface .getId (this);
    }
 
    get browser ()
    {
-      return X3D .getBrowser ("#browser")
+      return X3D .getBrowser ("#browser");
    }
 
    get filePath ()
    {
-      const worldURL = this .browser .currentScene .worldURL
+      const worldURL = this .browser .currentScene .worldURL;
 
       // New, unsaved or remote file.
       if (location .href === worldURL || !worldURL .startsWith ("file:"))
-         return undefined
+         return undefined;
 
-      return url .fileURLToPath (worldURL)
+      return url .fileURLToPath (worldURL);
    }
 
    set filePath (value)
    {
       const
          oldFilePath = this .filePath ?? this .fieldId ?? this .browser .getWorldURL (),
-         newWorldURL = value ? url .pathToFileURL (value) .href : location .href
+         newWorldURL = value ? url .pathToFileURL (value) .href : location .href;
 
-      this .browser .currentScene .setWorldURL (newWorldURL)
+      this .browser .currentScene .setWorldURL (newWorldURL);
 
-      this .updateFileConfigs (oldFilePath, value)
+      this .updateFileConfigs (oldFilePath, value);
    }
 
    get fileId ()
    {
       const
          location = new URL (window .location),
-         id       = new URL (location .searchParams .get ("url"))
+         id       = new URL (location .searchParams .get ("url"));
 
       if (id .protocol !== "id:")
-         return undefined
+         return undefined;
 
-      return id .pathname
+      return id .pathname;
    }
 
    /**
@@ -121,12 +121,12 @@ module .exports = class Interface
     */
    browserInitialized (event)
    {
-      this .fileConfig = this .#createFileConfig ()
+      this .fileConfig = this .#createFileConfig ();
 
-      this .browser .currentScene .setUserData (this .globalConfig, this .fileConfig)
+      this .browser .currentScene .setUserData (this .globalConfig, this .fileConfig);
 
-      this .configure ()
-      this .colorScheme (!! CSS .colorScheme .matches)
+      this .configure ();
+      this .colorScheme (!! CSS .colorScheme .matches);
    }
 
    /**
@@ -141,7 +141,7 @@ module .exports = class Interface
     */
    #createGlobalConfig (namespace = this .namespace)
    {
-      return new DataStorage (localStorage, namespace)
+      return new DataStorage (localStorage, namespace);
    }
 
    /**
@@ -152,11 +152,11 @@ module .exports = class Interface
    #createFileConfig (filePath, global = this .globalConfig)
    {
       if (!filePath && this .browser .currentScene === Interface .#initialScene)
-         return this .globalConfig .addNameSpace ("default.")
+         return this .globalConfig .addNameSpace ("default.");
 
-      filePath ??= this .filePath ?? this .fileId ?? this .browser .getWorldURL ()
+      filePath ??= this .filePath ?? this .fileId ?? this .browser .getWorldURL ();
 
-      return global .addNameSpace (md5 (filePath) + ".")
+      return global .addNameSpace (md5 (filePath) + ".");
    }
 
    /**
@@ -166,7 +166,7 @@ module .exports = class Interface
     */
    getFileConfig (executionContext)
    {
-      return executionContext .getUserData (this .globalConfig)
+      return executionContext .getUserData (this .globalConfig);
    }
 
    /**
@@ -178,35 +178,35 @@ module .exports = class Interface
    {
       const
          fromHash = `.${md5 (from)}.`,
-         toHash   = `.${md5 (to)}.`
+         toHash   = `.${md5 (to)}.`;
 
-      const config = new DataStorage (localStorage, "Sunrize.")
+      const config = new DataStorage (localStorage, "Sunrize.");
 
       for (const key of Object .keys (config))
       {
          if (!key .includes (fromHash))
-            continue
+            continue;
 
          const
             value  = config [key],
-            newKey = key .replace (fromHash, toHash)
+            newKey = key .replace (fromHash, toHash);
 
-         config [newKey] = value
+         config [newKey] = value;
       }
 
       for (const other of Interface .#interfaces)
       {
          const
             oldFileConfig = other .fileConfig,
-            newFileConfig = other .#createFileConfig (to)
+            newFileConfig = other .#createFileConfig (to);
 
-         newFileConfig .setDefaultValues (oldFileConfig .getDefaultValues ())
+         newFileConfig .setDefaultValues (oldFileConfig .getDefaultValues ());
 
-         other .fileConfig = newFileConfig
+         other .fileConfig = newFileConfig;
 
-         this .browser .currentScene .setUserData (other .globalConfig, newFileConfig)
+         this .browser .currentScene .setUserData (other .globalConfig, newFileConfig);
       }
    }
 
    configure () { }
-}
+};
