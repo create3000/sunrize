19181fe3de29d31bf9610b793bc55ec076883b14
Holger Seelig <holger.seelig@gmail.com>
2024-02-15 18:55:40 +0100

    Removed old options.

--- src/Application/Application.js
+++ src/Application/Application.js
@@ -67,10 +67,6 @@ module .exports = class Application
          expandExternProtoDeclarations: true,
          expandPrototypeInstances: true,
          expandInlineNodes: true,
-         primitiveQuality: "MEDIUM",
-         textureQuality: "MEDIUM",
-         rubberband: true,
-         timings: false,
       });
 
       Template .create (path .join (__dirname, "../assets/html/application-template.html"));
