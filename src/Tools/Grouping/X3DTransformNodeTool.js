5b5e94ca336fab132151c69dd185c542f474ce91
Holger Seelig <holger.seelig@gmail.com>
2024-02-28 11:56:37 +0100

    Use straighten.

--- src/Tools/Grouping/X3DTransformNodeTool.js
+++ src/Tools/Grouping/X3DTransformNodeTool.js
@@ -79,11 +79,6 @@ class X3DTransformNodeTool extends X3DChildNodeTool
    #initialScaleOrientation;
    #initialCenter;
 
-   prepareUndo ()
-   {
-      super .prepareUndo ();
-   }
-
    beginUndo ()
    {
       if (this .isHidden ())
