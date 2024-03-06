5469462d4a28be4d815a21be0da7f24cda8179ef
Holger Seelig <holger.seelig@gmail.com>
2024-03-06 11:09:44 +0100

    Use registerMetaDataCallback.

--- src/Tools/Lighting/DirectionalLightTool.js
+++ src/Tools/Lighting/DirectionalLightTool.js
@@ -11,9 +11,17 @@ class DirectionalLightTool extends X3DLightNodeTool
       this .tool .getField ("location") .addInterest ("set_location", this);
       this .tool .getField ("direction") .addReference (this .node ._direction);
 
+      this .registerMetaDataCallback (this, "DirectionalLight/location", this .set_meta_location .bind (this));
       this .getMetaData ("DirectionalLight/location", this .tool .location);
 
       this .tool .type = 0;
+   }
+
+   disposeTool ()
+   {
+      this .removeMetaDataCallback (this, "DirectionalLight/location");
+
+      super .disposeTool ();
    }
 
    beginUndo ()
@@ -21,9 +29,23 @@ class DirectionalLightTool extends X3DLightNodeTool
       this .undoSaveInitialValues (["location", "direction"]);
    }
 
+   #changing = false;
+
    set_location ()
    {
+      this .#changing = true;
+
       this .setMetaData ("DirectionalLight/location", this .tool .location);
+
+      this .#changing = false;
+   }
+
+   set_meta_location ()
+   {
+      if (this .#changing)
+         return;
+
+      this .getMetaData ("DirectionalLight/location", this .tool .location);
    }
 }
 
