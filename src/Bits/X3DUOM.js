f290f3b8c12b9de98b32e23e992c8c8b01a47637
Holger Seelig <holger.seelig@gmail.com>
2024-03-11 12:26:33 +0100

    Use pointerEvents field.

--- src/Bits/X3DUOM.js
+++ src/Bits/X3DUOM.js
@@ -9,11 +9,19 @@ const X3DUOM = $($.parseXML (fs .readFileSync (path .join (__dirname, "..", "ass
 X3DUOM .find (`AbstractNodeType[name=X3DOneSidedMaterialNode]`)
    .append (X3DUOM .find ("field[name=emissiveColor]") .first () .clone ());
 
+// pointerEvents
 X3DUOM .find (`AbstractNodeType[name=X3DShapeNode]`)
    .append ($("<field></field>")
       .attr ("name", "pointerEvents")
       .attr ("type", "SFBool")
       .attr ("accessType", "inputOutput")
-      .attr ("default", "true"));
+      .attr ("default", "true")
+      .attr ("description", "pointerEvents defines whether this Shape becomes target for pointer events."));
+
+X3DUOM .find (`ConcreteNode[name=Shape]`)
+   .append (X3DUOM .find ("field[name=pointerEvents]") .first () .clone ());
+
+X3DUOM .find (`ConcreteNode[name=ParticleSystem]`)
+   .append (X3DUOM .find ("field[name=pointerEvents]") .first () .clone ());
 
 module .exports = X3DUOM;
