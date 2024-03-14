674984c433580d8a92025c546d0e9ed3d96c53b2
Holger Seelig <holger.seelig@gmail.com>
2024-03-14 16:15:43 +0100

    Combine events.

--- src/Application/Document.js
+++ src/Application/Document.js
@@ -51,9 +51,8 @@ module .exports = class Document extends Interface
 
       $(window)
          .on ("focusin",  () => this .onfocus ())
-         .on ("focusout", () => this .onfocus ());
-
-      $(window) .on ("keydown", event => this .onkeydown (event));
+         .on ("focusout", () => this .onfocus ())
+         .on ("keydown", event => this .onkeydown (event));
 
       // File Menu
 
