d88c5a96f623d4dfc38cdf30476e0e5fd2418d82
Holger Seelig <holger.seelig@gmail.com>
2024-04-25 18:19:47 +0200

    Use substring.

--- src/Bits/Validate.js
+++ src/Bits/Validate.js
@@ -1,6 +1,6 @@
-"use strict"
+"use strict";
 
-const $ = require ("jquery")
+const $ = require ("jquery");
 
 /**
  *
@@ -10,50 +10,48 @@ const $ = require ("jquery")
  */
 $.fn.validate = function (regex, error)
 {
-   this
-      .off ("keydown.validate")
-      .off ("keyup.validate")
-      .off ("change.validate")
+   this .off (".validate");
 
-   let lastValue = this .val ()
+   let lastValue = this .val ();
 
    this .on ("keydown.validate", event =>
    {
       if (event .key .length > 1)
-         return
+         return;
 
       const
          start = this .prop ("selectionStart"),
          end   = this .prop ("selectionEnd"),
-         value = this .val () .substr (0, start) + event .key + this .val () .substr (end)
+         value = this .val () .substring (0, start) + event .key + this .val () .substring (end);
 
       if (value .match (regex))
-         return
+         return;
 
-      event .preventDefault ()
+      event .preventDefault ();
 
       if (typeof error === "function")
-         error .call (this, event)
+         error .call (this, event);
    })
    .on ("keyup.validate", (event) =>
    {
       // https://stackoverflow.com/questions/44962941/html-text-input-element-disable-mac-double-space-to-insert-period
 
       if (this .val () .match (regex))
-         return
+         return;
 
       const
          start = this .prop ("selectionStart"),
-         end   = this .prop ("selectionEnd")
+         end   = this .prop ("selectionEnd");
 
-      this .val (lastValue)
-      this .prop ("selectionStart", start)
-      this .prop ("selectionEnd",   end)
+      this
+         .val (lastValue)
+         .prop ("selectionStart", start)
+         .prop ("selectionEnd", end);
    })
    .on ("change.validate", (event) =>
    {
-      lastValue = this .val ()
-   })
+      lastValue = this .val ();
+   });
 
-   return this
-}
+   return this;
+};
