71eae07ca613eb623630ae593c1a91fffe78e1dd
Holger Seelig <holger.seelig@gmail.com>
2024-07-04 13:00:09 +0200

    Added Materials.x3d.

--- src/Editors/MaterialsLibrary.js
+++ src/Editors/MaterialsLibrary.js
@@ -5,15 +5,28 @@ const
    X3D         = require ("../X3D"),
    LibraryPane = require ("./LibraryPane");
 
+const fs = require ("fs");
+
 module .exports = class Materials extends LibraryPane
 {
    id          = "MATERIALS";
    description = "Materials";
 
-   update ()
+   #scene;
+
+   async update ()
    {
+      this .#scene ??= await this .browser .createX3DFromURL (new X3D .MFString (`file://${__dirname}/Materials.x3d`));
+
       // Clear output.
 
       this .output .empty ();
+
+      const materials = this .#scene .getExportedNode ("Materials");
+
+      for (const group of materials .children)
+      {
+         console .log (group .getNodeName ());
+      }
    }
 };
