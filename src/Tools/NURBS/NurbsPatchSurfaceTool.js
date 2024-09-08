4dc9614ddb5e90b9b5f9e6d7bd0b14696def019f
Holger Seelig <holger.seelig@gmail.com>
2024-09-08 10:31:34 +0200

    Added X3DNurbsSurfaceGeometryNodeTool.

--- src/Tools/NURBS/NurbsPatchSurfaceTool.js
+++ src/Tools/NURBS/NurbsPatchSurfaceTool.js
@@ -6,10 +6,8 @@ class NurbsPatchSurfaceTool extends X3DNurbsSurfaceGeometryNodeTool
 {
    async initializeTool (type)
    {
-      await Promise .all ([
-         super .initializeTool (type),
-         // super .loadTool (__dirname, "X3DNurbsSurfaceGeometryNode.x3d", "nurbsSurfaceGeometry"),
-      ]);
+      super .initializeTool (type),
+      // await super .loadTool (__dirname, "X3DNurbsSurfaceGeometryNode.x3d", "nurbsSurfaceGeometryTool");
    }
 }
 
