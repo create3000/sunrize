6ee2becdeff862637b83c816c556b2ddfb2438aa
Holger Seelig <holger.seelig@gmail.com>
2024-10-05 00:28:03 +0200

    Use X3DUOM from XML.

--- src/Bits/X3DUOM.js
+++ src/Bits/X3DUOM.js
@@ -8,35 +8,11 @@ const
    X3DUOM = $($.parseXML (fs .readFileSync (path .join (__dirname, "..", "assets", "X3DUOM.xml"), "utf-8"))),
    X_ITE  = $($.parseXML (fs .readFileSync (X3D .X3DUOM_PATH, "utf-8")));
 
+X3DUOM .find ("AbstractNodeTypes") .append (X_ITE .find ("AbstractNodeTypes"));
 X3DUOM .find ("ConcreteNodes") .append (X_ITE .find ("ConcreteNode"));
 
 // emissiveColor fix
 X3DUOM .find ("AbstractNodeType[name=X3DOneSidedMaterialNode]")
    .append (X3DUOM .find ("field[name=emissiveColor]") .first () .clone ());
-
-// pointerEvents
-X3DUOM .find ("AbstractNodeType[name=X3DShapeNode]")
-   .append ($(X3DUOM [0] .createElement ("field"))
-      .attr ("name", "pointerEvents")
-      .attr ("type", "SFBool")
-      .attr ("accessType", "inputOutput")
-      .attr ("default", "true")
-      .attr ("description", "pointerEvents defines whether this Shape becomes target for pointer events."));
-
-X3DUOM .find ("ConcreteNode[name=Shape]")
-   .append (X3DUOM .find ("field[name=pointerEvents]") .first () .clone ());
-
-X3DUOM .find ("ConcreteNode[name=ParticleSystem]")
-   .append (X3DUOM .find ("field[name=pointerEvents]") .first () .clone ());
-
-// damping
-X3DUOM .find ("ConcreteNode[name=BoundedPhysicsModel]")
-   .append ($(X3DUOM [0] .createElement ("field"))
-      .attr ("name", "damping")
-      .attr ("type", "SFFloat")
-      .attr ("accessType", "inputOutput")
-      .attr ("default", "1")
-      .attr ("minInclusive", "0")
-      .attr ("description", "Damping that particles experience in the event of a collision."));
 
 module .exports = X3DUOM;
