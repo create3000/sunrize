9ffa0cc6703f2266a6df4fa90d7c786060029d68
Holger Seelig <holger.seelig@gmail.com>
2024-11-02 09:15:01 +0100

    Added forge.config.js.

--- forge.config.js
+++ forge.config.js
@@ -1,47 +1,47 @@
 module.exports = {
-   "packagerConfig": {
-     "icon": "./src/assets/images/icon.png",
-     "dir": "./src",
-     "ignore": "^/(?!src|package\\.json|node_modules)",
-     "certificateFile": "./build/certificate.pfx",
-     "certificatePassword": "CREATE3000",
-     "extendInfo": "./src/assets/Info.plist",
-     "osxSign": {
-         "optionsForFile": (filePath) => {
-         // Here, we keep it simple and return a single entitlements.plist file.
-         // You can use this callback to map different sets of entitlements
-         // to specific files in your packaged app.
-         return {
-           entitlements: "src/assets/entitlements.plist"
-         };
-       }
-     }
-   },
-   "makers": [
-     {
-       "name": "@electron-forge/maker-squirrel"
-     },
-     {
-       "name": "@electron-forge/maker-dmg"
-     },
-     {
-       "name": "@electron-forge/maker-deb"
-     },
-     {
-       "name": "@electron-forge/maker-rpm"
-     }
-   ],
-   "publishers": [
-     {
-       "name": "@electron-forge/publisher-github",
-       "config": {
-         "repository": {
-           "owner": "create3000",
-           "name": "sunrize"
-         },
-         "prerelease": false,
-         "draft": false
-       }
-     }
-   ]
- };
+  "packagerConfig": {
+    "icon": "./src/assets/images/icon.png",
+    "dir": "./src",
+    "ignore": "^/(?!src|package\\.json|node_modules)",
+    "certificateFile": "./build/certificate.pfx",
+    "certificatePassword": "CREATE3000",
+    "extendInfo": "./src/assets/Info.plist",
+    "osxSign": {
+      "optionsForFile": (filePath) => {
+        // Here, we keep it simple and return a single entitlements.plist file.
+        // You can use this callback to map different sets of entitlements
+        // to specific files in your packaged app.
+        return {
+          "entitlements": "src/assets/entitlements.plist"
+        };
+      }
+    }
+  },
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel"
+    },
+    {
+      "name": "@electron-forge/maker-dmg"
+    },
+    {
+      "name": "@electron-forge/maker-deb"
+    },
+    {
+      "name": "@electron-forge/maker-rpm"
+    }
+  ],
+  "publishers": [
+    {
+      "name": "@electron-forge/publisher-github",
+      "config": {
+        "repository": {
+          "owner": "create3000",
+          "name": "sunrize"
+        },
+        "prerelease": false,
+        "draft": false
+      }
+    }
+  ]
+};
