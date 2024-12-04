8caea279a56442c32cecf9269a24209187cd65de
create3000 <holger.seelig@gmail.com>
2024-12-04 15:11:58 +0100

    Pass args as base64.

--- bin/sunrize.js
+++ bin/sunrize.js
@@ -4,11 +4,17 @@
 const os = require ("os");
 const path = require ("path");
 const { spawn } = require ("child_process");
+const cwd = process .cwd ();
 const cmd = os .platform () === "win32" ? "npm.cmd" : "npm";
+const args = process .argv .slice (2);
 
 process .chdir (path .resolve (__dirname, ".."));
 
-const p = spawn (cmd, ["start", "--silent", "--", ... process .argv .slice (2)], { shell: true });
+const p = spawn (cmd, ["start", "--silent", "--", btoa (JSON .stringify (args))],
+{
+   shell: true,
+   cwd: cwd,
+});
 
 p .stdout .pipe (process .stdout);
 p .stderr .pipe (process .stderr);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 "use strict";
 
+process .argv = process .argv .concat (JSON .parse (atob (process .argv .pop ())));
+
 const app = require ("./Application/Application") .run ();
