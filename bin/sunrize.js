68146ce9324295316c722955329bcd29c60d600f
Holger Seelig <holger.seelig@gmail.com>
2024-12-05 12:41:45 +0100

    Fixed cwd handling.

--- bin/sunrize.js
+++ bin/sunrize.js
@@ -6,15 +6,11 @@ const path = require ("path");
 const { spawn } = require ("child_process");
 const cwd = process .cwd ();
 const cmd = os .platform () === "win32" ? "npm.cmd" : "npm";
-const args = process .argv .slice (2);
+const args = [... process .argv .slice (2), cwd];
 
 process .chdir (path .resolve (__dirname, ".."));
 
-const p = spawn (cmd, ["start", "--silent", "--", btoa (JSON .stringify (args))],
-{
-   shell: true,
-   cwd: cwd,
-});
+const p = spawn (cmd, ["start", "--silent", "--", btoa (JSON .stringify (args))], { shell: true });
 
 p .stdout .pipe (process .stdout);
 p .stderr .pipe (process .stderr);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 "use strict";
 
 if (process .argv .length > 2)
+{
    process .argv = process .argv .concat (JSON .parse (atob (process .argv .pop ())));
+   process .chdir (process .argv .pop ());
+}
 
 const app = require ("./Application/Application") .run ();
