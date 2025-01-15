1b7c9b45f5ad373c51bc40e286bab15ba3f665eb
Holger Seelig <holger.seelig@gmail.com>
2025-01-15 16:01:38 +0100

    Added Cylinder.toPrimitive.

---
+++ src/Components/Geometry3D/Cylinder.js
@@ -0,0 +1,52 @@
+const X3D = require ("../../X3D");
+
+X3D .Cylinder .prototype .toPrimitive = function (executionContext = this .getExecutionContext ())
+{
+   const
+      browser   = this .getBrowser (),
+      geometry  = browser .getCylinderOptions () .getSideGeometry () .copy (executionContext),
+      radius    = this ._radius .getValue (),
+      height1_2 = this ._height .getValue () / 2;
+
+   geometry ._texCoord = geometry ._texCoord .getValue () .copy (executionContext);
+   geometry ._normal   = geometry ._normal   .getValue () .copy (executionContext);
+   geometry ._coord    = geometry ._coord    .getValue () .copy (executionContext);
+
+   for (const point of geometry ._coord .point)
+   {
+      point .x *= radius;
+      point .y *= height1_2;
+      point .z *= radius;
+   }
+
+   if (this ._top .getValue ())
+   {
+      for (const index of browser .getCylinderOptions () .getTopGeometry () ._texCoordIndex)
+         geometry ._texCoordIndex .push (index);
+
+      for (const index of browser .getCylinderOptions () .getTopGeometry () ._normalIndex)
+         geometry ._normalIndex .push (index);
+
+      for (const index of browser .getCylinderOptions () .getTopGeometry () ._coordIndex)
+         geometry ._coordIndex .push (index);
+   }
+
+   if (this ._bottom .getValue ())
+   {
+      for (const index of browser .getCylinderOptions () .getBottomGeometry () ._texCoordIndex)
+         geometry ._texCoordIndex .push (index);
+
+      for (const index of browser .getCylinderOptions () .getBottomGeometry () ._normalIndex)
+         geometry ._normalIndex .push (index);
+
+      for (const index of browser .getCylinderOptions () .getBottomGeometry () ._coordIndex)
+         geometry ._coordIndex .push (index);
+   }
+
+   geometry ._texCoord .getValue () .setup ();
+   geometry ._normal   .getValue () .setup ();
+   geometry ._coord    .getValue () .setup ();
+   geometry .setup ();
+
+   return geometry;
+};
