30875a3811b55bb8becd901aa832e008456035f9
Holger Seelig <holger.seelig@gmail.com>
2025-01-18 18:00:35 +0100

    Add toPrimitive method to NurbsSweptSurface and NurbsSwungSurface for geometry conversion.

--- src/Components/NURBS/NurbsSweptSurface.js
+++ src/Components/NURBS/NurbsSweptSurface.js
@@ -6,4 +6,17 @@ Object .assign (X3D .NurbsSweptSurface .prototype,
    {
       this .getTrajectoryCurve () ?.traverse (type, renderObject);
    },
+   toPrimitive (executionContext = this .getExecutionContext ())
+   {
+      const geometry = this .toIndexedFaceSet (executionContext,
+      {
+         texCoord: true,
+      });
+
+      geometry ._solid       = this ._solid;
+      geometry ._ccw         = this ._ccw;
+      geometry ._creaseAngle = Math .PI;
+
+      return geometry;
+   },
 });
