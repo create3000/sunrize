6f87872e2205c06dce45cc513cd155485945b61c
Holger Seelig <holger.seelig@gmail.com>
2025-01-19 00:05:33 +0100

    Added X3DComposedGeometryNode.toPrimitive.

--- src/Components/Rendering/X3DComposedGeometryNode.js
+++ src/Components/Rendering/X3DComposedGeometryNode.js
@@ -4,14 +4,40 @@ Object .assign (X3D .X3DComposedGeometryNode .prototype,
 {
    toPrimitive (executionContext = this .getExecutionContext)
    {
-      return this .toIndexedFaceSet (executionContext,
+      const geometry = executionContext .createNode ("IndexedFaceSet", false);
+
+      geometry ._solid           = this ._solid;
+      geometry ._ccw             = this ._ccw;
+      geometry ._creaseAngle     = Math .PI;
+      geometry ._colorPerVertex  = this ._colorPerVertex;
+      geometry ._normalPerVertex = this ._normalPerVertex;
+
+      geometry ._attrib   = this ._attrib;
+      geometry ._fogCoord = this ._fogCoord;
+      geometry ._color    = this ._color;
+      geometry ._tangent  = this ._tangent;
+      geometry ._normal   = this ._normal;
+      geometry ._coord    = this ._coord;
+
+      let
+         verticesPerPolygon = this .getVerticesPerPolygon (),
+         numVertices        = this .getNumVertices ();
+
+      // Set size to a multiple of vertexCount.
+      numVertices -= numVertices % verticesPerPolygon;
+
+      for (let i = 0; i < numVertices; ++ i)
       {
-         double: !!X3D .X3DCast (X3D .X3DConstants .CoordinateDouble, this ._coord),
-         fogCoord: !!this ._fogCoord .getValue (),
-         color: !!this ._color .getValue (),
-         texCoord: !!this ._texCoord .getValue (),
-         tangent: !!this ._tangent .getValue (),
-         normal: !!this ._normal .getValue (),
-      });
+         const index = this .getPolygonIndex (i);
+
+         geometry ._coordIndex .push (index);
+
+         if (i % verticesPerPolygon === verticesPerPolygon - 1)
+            geometry ._coordIndex .push (-1);
+      }
+
+      geometry .setup ();
+
+      return geometry;
    },
 });
