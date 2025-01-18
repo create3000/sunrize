8901dfdca6696fa65052112bc5d166e612117eda
Holger Seelig <holger.seelig@gmail.com>
2025-01-19 00:24:02 +0100

    Added spaces.

--- src/Components/Geometry2D/TriangleSet2D.js
+++ src/Components/Geometry2D/TriangleSet2D.js
@@ -20,7 +20,7 @@ Object .assign (X3D .TriangleSet2D .prototype,
       for (let i = 0, length = vertices .length; i < length; i += 4)
       {
          geometry ._texCoord .point .push (new X3D .SFVec2f (texCoords [i], texCoords [i + 1]));
-         geometry ._coord    .point .push (new X3D .SFVec3f (vertices [i],  vertices  [i + 1], 0));
+         geometry ._coord    .point .push (new X3D .SFVec3f (vertices  [i], vertices  [i + 1], 0));
       }
 
       geometry ._texCoord .getValue () .setup ();
