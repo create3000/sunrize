2f8d6791fc9f3ec550fa9b9ddb1cb6c0cde9231d
Holger Seelig <holger.seelig@gmail.com>
2025-03-05 09:04:43 +0100

    Handle isCollisionObject.

--- src/Components/Grouping/StaticGroup.js
+++ src/Components/Grouping/StaticGroup.js
@@ -31,11 +31,12 @@ Object .assign (Object .setPrototypeOf (StaticGroup .prototype, X3D .X3DChildNod
       this .groupNode .setup ();
 
       // Connect after Group setup.
-      this .groupNode ._isPointingObject .addFieldInterest (this ._isPointingObject);
-      this .groupNode ._isCameraObject   .addFieldInterest (this ._isCameraObject);
-      this .groupNode ._isPickableObject .addFieldInterest (this ._isPickableObject);
-      this .groupNode ._isShadowObject   .addFieldInterest (this ._isShadowObject);
-      this .groupNode ._isVisibleObject  .addFieldInterest (this ._isVisibleObject);
+      this .groupNode ._isPointingObject  .addFieldInterest (this ._isPointingObject);
+      this .groupNode ._isCameraObject    .addFieldInterest (this ._isCameraObject);
+      this .groupNode ._isPickableObject  .addFieldInterest (this ._isPickableObject);
+      this .groupNode ._isCollisionObject .addFieldInterest (this ._isCollisionObject);
+      this .groupNode ._isShadowObject    .addFieldInterest (this ._isShadowObject);
+      this .groupNode ._isVisibleObject   .addFieldInterest (this ._isVisibleObject);
 
       this .setPointingObject (this .groupNode .isPointingObject ());
       this .setCameraObject   (this .groupNode .isCameraObject ());
