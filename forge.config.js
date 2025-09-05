c53eec1b11b484c64163aead05ed16dc6ef49b18
Holger Seelig <holger.seelig@gmail.com>
2025-09-05 18:38:40 +0200

    Remove commented-out maker-dmg entry and adjust maker-zip entry in forge configuration

--- forge.config.js
+++ forge.config.js
@@ -19,16 +19,20 @@ module.exports = {
   },
   "makers": [
     {
-      "name": "@electron-forge/maker-squirrel"
+      "name": "@electron-forge/maker-squirrel",
+    },
+    // {
+    //   "name": "@electron-forge/maker-dmg",
+    // },
+    {
+      "name": '@electron-forge/maker-zip',
+      "platforms": ['darwin'],
     },
     {
-      "name": "@electron-forge/maker-dmg"
+      "name": "@electron-forge/maker-deb",
     },
     {
-      "name": "@electron-forge/maker-deb"
-    },
-    {
-      "name": "@electron-forge/maker-rpm"
+      "name": "@electron-forge/maker-rpm",
     }
   ],
   "publishers": [
