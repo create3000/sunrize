b8ff848bec63ec6e321de8ef1114fa0c25f8122f
Holger Seelig <holger.seelig@gmail.com>
2025-09-05 14:58:39 +0200

    Update files.

--- build/download.js
+++ build/download.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 
-const
-	fs                 = require ("fs"),
-	{ systemSync, sh } = require ("shell-tools");
+const { systemSync } = require ("shell-tools");
 
 function main ()
 {
    make ();
-   docs ();
-   merge ();
 }
 
 function make ()
@@ -18,32 +14,6 @@ function make ()
 
    systemSync (`npm run github -- --platform darwin`);
    systemSync (`npm run github -- --platform win32`);
-}
-
-function docs ()
-{
-   const version = sh (`npm pkg get version | sed 's/"//g'`) .trim ();
-
-   // config
-	let config = sh (`cat 'docs/_config.yml'`);
-
-	config = config .replace (/\bversion:\s*[\d\.]+/sg, `version: ${version}`);
-
-	fs .writeFileSync ("docs/_config.yml", config);
-}
-
-function merge ()
-{
-	// commit
-	systemSync (`git add -A`);
-	systemSync (`git commit -am 'Updated downloads.'`);
-	systemSync (`git push origin`);
-
-	// // merge
-	// systemSync (`git checkout main`);
-	// systemSync (`git merge development`);
-	// systemSync (`git push origin`);
-	// systemSync (`git checkout development`);
 }
 
 main ();
