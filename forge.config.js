68ad02e5dac95759e2940ab0849f6ea8073de51a
Holger Seelig <holger.seelig@gmail.com>
2025-09-05 18:39:02 +0200

    Fix formatting of maker-zip entry in forge configuration

--- forge.config.js
+++ forge.config.js
@@ -25,8 +25,8 @@ module.exports = {
     //   "name": "@electron-forge/maker-dmg",
     // },
     {
-      "name": '@electron-forge/maker-zip',
-      "platforms": ['darwin'],
+      "name": "@electron-forge/maker-zip",
+      "platforms": ["darwin"],
     },
     {
       "name": "@electron-forge/maker-deb",
