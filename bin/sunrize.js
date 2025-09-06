6f1974ea7d3c42661ec7a578ef99fffddd321077
Holger Seelig <holger.seelig@gmail.com>
2025-09-06 13:48:49 +0200

    Only span shell on windows.

--- bin/sunrize.js
+++ bin/sunrize.js
@@ -4,18 +4,19 @@
 const os = require ("os");
 const path = require ("path");
 const { spawn } = require ("child_process");
+const win = os .platform () === "win32";
 const cwd = process .cwd ();
-const cmd = os .platform () === "win32" ? "npm.cmd" : "npm";
+const cmd = win ? "npm.cmd" : "npm";
 const args = process .argv .slice (2) .map (escapeQuotes);
 
 process .chdir (path .resolve (__dirname, ".."));
 
-const p = spawn (cmd, ["start", "--silent", "--", ... args], { cwd, shell: true });
+const p = spawn (cmd, ["start", "--silent", "--", ... args], { cwd, shell: win });
 
 p .stdout .pipe (process .stdout);
 p .stderr .pipe (process .stderr);
 
 function escapeQuotes (arg)
 {
-   return `"${arg .replaceAll ('"', os .platform () === "win32" ? '"""': '\\"')}"`;
+   return win ? `"${arg .replaceAll ('"', '"""')}"` : arg;
 }
