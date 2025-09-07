2bfe7bca23c99e2a477743207566ea3308446acc
Holger Seelig <holger.seelig@gmail.com>
2025-09-07 10:45:32 +0200

    Added publish-to-github script.

---
+++ build/publish-to-github.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+"use strict";
+
+const { systemSync } = require ("shell-tools");
+
+function main ()
+{
+   systemSync (`npm up`);
+   systemSync (`npm i -D electron`);
+   systemSync (`electron-forge publish`);
+   systemSync (`npm i -P electron`);
+}
+
+main ();
--- build/release.js
+++ build/release.js
@@ -6,71 +6,71 @@ const { sh, systemSync } = require ("shell-tools");
 
 function main ()
 {
-	if (sh (`git branch --show-current`) !== "development\n")
-	{
-		console .error ("Wrong branch, must be development, cannot release version!");
-		process .exit (1);
-	}
+   if (sh (`git branch --show-current`) !== "development\n")
+   {
+      console .error ("Wrong branch, must be development, cannot release version!");
+      process .exit (1);
+   }
 
-	// merge
-	systemSync (`git checkout main`);
-	systemSync (`git merge development`);
+   // merge
+   systemSync (`git checkout main`);
+   systemSync (`git merge development`);
 
-	// version
-	const
-		name    = sh (`node -p "require('./package.json').name"`) .trim (),
-		online  = sh (`npm view ${name} version`) .trim ();
+   // version
+   const
+      name    = sh (`node -p "require('./package.json').name"`) .trim (),
+      online  = sh (`npm view ${name} version`) .trim ();
 
-	if (sh (`npm pkg get version | sed 's/"//g'`) .trim () === online)
-		systemSync (`npm version patch --no-git-tag-version --force`);
+   if (sh (`npm pkg get version | sed 's/"//g'`) .trim () === online)
+      systemSync (`npm version patch --no-git-tag-version --force`);
 
-	const version = sh (`npm pkg get version | sed 's/"//g'`) .trim ();
+   const version = sh (`npm pkg get version | sed 's/"//g'`) .trim ();
 
-	console .log (`NPM version ${online}`);
-	console .log (`New version ${version}`);
+   console .log (`NPM version ${online}`);
+   console .log (`New version ${version}`);
 
-	while (systemSync (`npm i x_ite@latest`))
-		systemSync (`countdown 60s`);
+   while (systemSync (`npm i x_ite@latest`))
+      systemSync (`countdown 60s`);
 
-	while (systemSync (`npm i x3d-traverse@latest`))
-		systemSync (`countdown 60s`);
+   while (systemSync (`npm i x3d-traverse@latest`))
+      systemSync (`countdown 60s`);
 
-	// x3duom
-	systemSync (`npm run X3DUOM`);
+   // x3duom
+   systemSync (`npm run X3DUOM`);
 
-	// docs
-	docs (version);
+   // docs
+   docs (version);
 
-	// commit
-	systemSync (`git add -A`);
-	systemSync (`git commit -am 'Published version ${version}'`);
-	systemSync (`git push origin`);
+   // commit
+   systemSync (`git add -A`);
+   systemSync (`git commit -am 'Published version ${version}'`);
+   systemSync (`git push origin`);
 
-	// tags are created with github-publisher
-	// tag
-	systemSync (`git tag ${version}`);
-	systemSync (`git push origin --tags`);
+   // tags are created with github-publisher
+   // tag
+   systemSync (`git tag ${version}`);
+   systemSync (`git push origin --tags`);
 
-	// npm
-	systemSync (`npm publish`);
+   // npm
+   systemSync (`npm publish`);
 
-	// merge development into main
-	systemSync (`git checkout development`);
-	systemSync (`git merge main`);
-	systemSync (`git push origin`);
+   // merge development into main
+   systemSync (`git checkout development`);
+   systemSync (`git merge main`);
+   systemSync (`git push origin`);
 
-	// // package
-	// systemSync (`npm run download`);
+   // // package
+   // systemSync (`npm run download`);
 }
 
 function docs (version)
 {
    // config
-	let config = sh (`cat 'docs/_config.yml'`);
+   let config = sh (`cat 'docs/_config.yml'`);
 
-	config = config .replace (/\bversion:\s*[\d\.]+/sg, `version: ${version}`);
+   config = config .replace (/\bversion:\s*[\d\.]+/sg, `version: ${version}`);
 
-	fs .writeFileSync ("docs/_config.yml", config);
+   fs .writeFileSync ("docs/_config.yml", config);
 }
 
 main ();
