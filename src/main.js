cbdad804200ec02c85e90ca5ba99c924e07d4aab
Holger Seelig <holger.seelig@gmail.com>
2025-09-07 10:20:15 +0200

    Added comments.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 "use strict";
 
+// Restore cwd.
+
 if (process .argv .at (-2) === "--cwd")
    process .chdir (process .argv .at (-1));
+
+// Run application.
 
 require ("./Application/Application") .run ();
