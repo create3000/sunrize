28cdc648574b5a41096e55110ad3b6797cd8c130
Holger Seelig <holger.seelig@gmail.com>
2025-09-15 00:14:52 +0200

    Added constants to loading-gif script.

--- build/loading-gif.js
+++ build/loading-gif.js
@@ -3,18 +3,22 @@
 
 const { systemSync } = require ("shell-tools");
 
+const
+   NUM_FRAMES = 3,
+   DELAY      = 40;
+
 function main ()
 {
-   for (let i = 1; i < 4; ++ i)
+   for (let i = 1; i <= NUM_FRAMES; ++ i)
    {
       systemSync (`npx --yes x3d-image -s "445x445" -e .png -i src/assets/images/loading/loading${i}.x3d`);
       systemSync (`magick src/assets/images/loading/loading${i}.png src/assets/images/loading${i}.gif`);
       systemSync (`rm src/assets/images/loading/loading${i}.png`);
    }
 
-   systemSync (`magick -delay 40 src/assets/images/loading3.gif src/assets/images/loading2.gif src/assets/images/loading1.gif -loop 0 src/assets/images/loading.gif`)
+   systemSync (`magick -delay ${DELAY} src/assets/images/loading3.gif src/assets/images/loading2.gif src/assets/images/loading1.gif -loop 0 src/assets/images/loading.gif`)
 
-   for (let i = 1; i < 4; ++ i)
+   for (let i = 1; i <= NUM_FRAMES; ++ i)
       systemSync (`rm src/assets/images/loading${i}.gif`);
 }
 
