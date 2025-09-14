e04160521d9ddd4cef58bd336531f7bc5e9fa5e2
Holger Seelig <holger.seelig@gmail.com>
2025-09-14 18:37:49 +0200

    Added loading.gif.

---
+++ build/loading-gif.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+"use strict";
+
+const { systemSync } = require ("shell-tools");
+
+function main ()
+{
+   systemSync (`npx --yes x3d-image -s "445x445" -e .png -i src/assets/images/loading.x3d`);
+   systemSync (`magick src/assets/images/loading.png src/assets/images/loading.gif`);
+   systemSync (`rm src/assets/images/loading.png`);
+}
+
+main ();
