52db7dc22bb494eaccbb6140f6fee02143225119
Holger Seelig <holger.seelig@gmail.com>
2025-09-14 18:51:41 +0200

    Added loading folder.

--- build/loading-gif.js
+++ build/loading-gif.js
@@ -5,9 +5,9 @@ const { systemSync } = require ("shell-tools");
 
 function main ()
 {
-   systemSync (`npx --yes x3d-image -s "445x445" -e .png -i src/assets/images/loading.x3d`);
-   systemSync (`magick src/assets/images/loading.png src/assets/images/loading.gif`);
-   systemSync (`rm src/assets/images/loading.png`);
+   systemSync (`npx --yes x3d-image -s "445x445" -e .png -i src/assets/images/loading/loading.x3d`);
+   systemSync (`magick src/assets/images/loading/loading.png src/assets/images/loading.gif`);
+   systemSync (`rm src/assets/images/loading/loading.png`);
 }
 
 main ();
