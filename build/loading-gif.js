117e2d1771ee1c46a625a163f171c51cf7faf544
Holger Seelig <holger.seelig@gmail.com>
2025-09-15 02:43:26 +0200

    Optimize loading-gif.js.

--- build/loading-gif.js
+++ build/loading-gif.js
@@ -11,19 +11,25 @@ function main ()
 {
    for (let i = 1; i <= NUM_FRAMES; ++ i)
    {
-      console .log (`Frame: ${i}`);
       systemSync (`cp src/assets/images/loading/loading.x3d src/assets/images/loading/loading${i}.x3d`)
       systemSync (`sed -i '' "s/name='frame'/name='frame' value='${i - 1}'/" src/assets/images/loading/loading${i}.x3d`)
-      systemSync (`npx --yes x3d-image -s "445x445" -e .png -i src/assets/images/loading/loading${i}.x3d`);
+   }
+
+   const x3d = Array .from ({ length: NUM_FRAMES }, (_, i) => `src/assets/images/loading/loading${i + 1}.x3d`) .join (" ");
+
+   systemSync (`npx --yes x3d-image -s "445x445" -e .png -i ${x3d} -l`);
+
+   for (let i = 1; i <= NUM_FRAMES; ++ i)
+   {
       systemSync (`magick src/assets/images/loading/loading${i}.png src/assets/images/loading${i}.gif`);
       systemSync (`rm src/assets/images/loading/loading${i}.x3d`);
       systemSync (`rm src/assets/images/loading/loading${i}.png`);
    }
 
-   const gifs = Array .from ({ length: NUM_FRAMES }, (_, i) => `src/assets/images/loading${i + 1}.gif`) .join (" ");
+   const gif = Array .from ({ length: NUM_FRAMES }, (_, i) => `src/assets/images/loading${i + 1}.gif`) .join (" ");
 
-   systemSync (`magick -delay ${DELAY} ${gifs} -loop 0 src/assets/images/loading.gif`)
-   systemSync (`rm ${gifs}`);
+   systemSync (`magick -delay ${DELAY} ${gif} -loop 0 src/assets/images/loading.gif`)
+   systemSync (`rm ${gif}`);
 }
 
 main ();
