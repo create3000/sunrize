721fa746b23f17f517bf1f08723a694cd408912c
Holger Seelig <holger.seelig@gmail.com>
2025-09-15 11:29:10 +0200

    Optimize loading-gif.js.

--- build/loading-gif.js
+++ build/loading-gif.js
@@ -7,39 +7,37 @@ const
    NUM_FRAMES = 360 / 3,
    DELAY      = 4;
 
+const
+   images  = `src/assets/images`,
+   loading = `src/assets/images/loading`;
+
 function main ()
 {
    // Create X3D frames.
 
    for (let i = 1; i <= NUM_FRAMES; ++ i)
    {
-      systemSync (`cp src/assets/images/loading/loading.x3d src/assets/images/loading/loading${i}.x3d`)
-      systemSync (`sed -i "" "s/name='frame'/name='frame' value='${i - 1}'/" src/assets/images/loading/loading${i}.x3d`)
+      systemSync (`cp ${loading}/loading.x3d ${loading}/loading${i}.x3d`)
+      systemSync (`sed -i "" "s/name='frame'/name='frame' value='${i - 1}'/" ${loading}/loading${i}.x3d`)
    }
 
-   // Create GIF frames.
+   // Create PNG frames.
 
-   const x3d = Array .from ({ length: NUM_FRAMES }, (_, i) => `src/assets/images/loading/loading${i + 1}.x3d`) .join (" ");
+   const x3d = Array .from ({ length: NUM_FRAMES }, (_, i) => `${loading}/loading${i + 1}.x3d`) .join (" ");
 
    systemSync (`npx --yes x3d-image -s "445x445" -e .png -i ${x3d} -l`);
-
-   for (let i = 1; i <= NUM_FRAMES; ++ i)
-   {
-      systemSync (`magick src/assets/images/loading/loading${i}.png src/assets/images/loading${i}.gif`);
-      systemSync (`rm src/assets/images/loading/loading${i}.x3d`);
-      systemSync (`rm src/assets/images/loading/loading${i}.png`);
-   }
+   systemSync (`rm ${x3d}`);
 
    // Create GIF animation.
 
-   const gif = Array .from ({ length: NUM_FRAMES }, (_, i) => `src/assets/images/loading${i + 1}.gif`) .join (" ");
+   const png = Array .from ({ length: NUM_FRAMES }, (_, i) => `${loading}/loading${i + 1}.png`) .join (" ");
 
-   systemSync (`magick -delay ${DELAY} ${gif} -loop 0 src/assets/images/loading.gif`);
-   systemSync (`rm ${gif}`);
+   systemSync (`magick -delay ${DELAY} ${png} -alpha remove -layers OptimizePlus -loop 0 ${images}/loading.gif`);
+   systemSync (`rm ${png}`);
 
    // Optimize GIF.
 
-   systemSync (`gifsicle --batch -O3 --colors 256 src/assets/images/loading.gif`);
+   systemSync (`gifsicle --batch -O3 --colors 256 ${images}/loading.gif`);
 }
 
 main ();
