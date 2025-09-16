81f67f7f5c077445e0b51d51389e4d24a18adc4b
Holger Seelig <holger.seelig@gmail.com>
2025-09-16 12:56:00 +0200

    Added vertical splitter.

--- src/Editors/AnimationEditor.js
+++ src/Editors/AnimationEditor.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const
-   Interface = require ("../Application/Interface");
+   $            = require ("jquery"),
+   Interface    = require ("../Application/Interface"),
+   Splitter     = require ("../Controls/Splitter");
 
 module .exports = class AnimationEditor extends Interface
 {
@@ -11,8 +13,33 @@ module .exports = class AnimationEditor extends Interface
 
       this .animationEditor = element;
 
-      this .animationEditor .text ("Animation Editor");
+      this .verticalSplitter = $("<div></div>")
+         .attr ("id", "animation-editor-left")
+         .addClass (["animation-editor-left", "vertical-splitter"])
+         .appendTo (this .animationEditor);
+
+      this .verticalSplitterLeft = $("<div></div>")
+         .addClass ("vertical-splitter-left")
+         .css ("width", "30%")
+         .appendTo (this .verticalSplitter);
+
+      this .verticalSplitterRight = $("<div></div>")
+         .addClass ("vertical-splitter-right")
+         .css ("width", "70%")
+         .appendTo (this .verticalSplitter);
+
+      this .vSplitter = new Splitter (this .verticalSplitter, "vertical");
+
+      this .verticalSplitter .on ("position", () => this .onSplitterPosition ());
 
       this .setup ();
+   }
+
+   onSplitterPosition ()
+   {
+      if (this .vSplitter .position)
+         this .toggleSidebarButton .addClass ("active");
+      else
+         this .toggleSidebarButton .removeClass ("active");
    }
 }
