2541cff2c2d7c187b61b7b2ab5c3e96f789aae01
Holger Seelig <holger.seelig@gmail.com>
2025-10-11 19:08:00 +0200

    Refactor element selection logic to use :is() and :not() for improved readability

--- src/Editors/OutlineEditor.js
+++ src/Editors/OutlineEditor.js
@@ -85,7 +85,7 @@ module .exports = class OutlineEditor extends OutlineRouteGraph
       if (!element .is (".manually"))
          this .sceneGraph .find (".manually") .removeClass ("manually");
 
-      if (element .is (".externproto, .proto, .proto-scene, .node, .field, .imported-node, .exported-node") && !element .is (".manually"))
+      if (element .is (":is(.externproto, .proto, .proto-scene, .node, .field, .imported-node, .exported-node):not(.manually)"))
          this .selectPrimaryElement (element);
 
       const
