98398f518fb11200e4157f4c0c9391907dfee578
Holger Seelig <holger.seelig@gmail.com>
2026-03-03 12:56:29 +0100

    Added InlineGeometryTool.

---
+++ src/Tools/Networking/InlineGeometryTool.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const X3DGeometryNodeTool = require ("../Rendering/X3DGeometryNodeTool");
+
+class InlineGeometryTool extends X3DGeometryNodeTool { }
+
+module .exports = InlineGeometryTool;
