f2349bc13f6d437e927b1d9252e01aec30b18520
Holger Seelig <holger.seelig@gmail.com>
2026-03-14 11:13:13 +0100

    Add radio type and checked state to viewpoint submenu items.

--- src/Application/Document.js
+++ src/Application/Document.js
@@ -1266,9 +1266,9 @@ Viewpoint {
          {
             label: _("Viewpoints"),
             submenu: viewpoints .filter ((_, index) => index > 0) .map ((viewpointNode, index) => ({
+               label: `${viewpointNode ._description .getValue () || viewpointNode .getDisplayName () || `VP${index + 1}}`}`,
                type: "radio",
                checked: viewpointNode ._isBound .getValue (),
-               label: `${viewpointNode ._description .getValue () || viewpointNode .getDisplayName () || `VP${index + 1}}`}`,
                args: ["bindViewpoint", index + 1],
             })),
          },
