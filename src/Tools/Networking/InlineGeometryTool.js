00a582730f25e368518ac6a249afa91227a58cc5
Holger Seelig <holger.seelig@gmail.com>
2026-04-08 23:13:36 +0200

    Updated InlineGeometryTool.

--- src/Tools/Networking/InlineGeometryTool.js
+++ src/Tools/Networking/InlineGeometryTool.js
@@ -2,6 +2,49 @@
 
 const X3DGeometryNodeTool = require ("../Rendering/X3DGeometryNodeTool");
 
-class InlineGeometryTool extends X3DGeometryNodeTool { }
+class InlineGeometryTool extends X3DGeometryNodeTool
+{
+   #tool;
+
+   constructor (node)
+   {
+      super (node)
+
+      node ._loadState .addInterest ("set_toolLoadState", this);
+
+      this .set_toolLoadState ();
+   }
+
+   set_toolLoadState ()
+   {
+      try
+      {
+         this .#tool = this .node .getInnerNode () .addTool ();
+
+         this .#tool .toolPointingEnabled = false;
+      }
+      catch
+      {
+         this .#tool = null;
+      }
+   }
+
+   disposeTool ()
+   {
+      try
+      {
+         this .node .getInnerNode () .removeTool ();
+      }
+      catch
+      { }
+
+      super .disposeTool ();
+   }
+
+   getInnerNode ()
+   {
+      return this .#tool;
+   }
+}
 
 module .exports = InlineGeometryTool;
