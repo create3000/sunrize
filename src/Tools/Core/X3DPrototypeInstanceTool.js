15d519a81950f2810c69f81b78bd1157ded86424
Holger Seelig <holger.seelig@gmail.com>
2026-04-09 09:57:06 +0200

    Handle rootNodes changes.

--- src/Tools/Core/X3DPrototypeInstanceTool.js
+++ src/Tools/Core/X3DPrototypeInstanceTool.js
@@ -4,24 +4,37 @@ const X3DNodeTool = require ("./X3DNodeTool");
 
 class X3DPrototypeInstanceTool extends X3DNodeTool
 {
+   #tool;
+
    constructor (node)
    {
       super (node)
 
+      node .getBody () .rootNodes .addInterest ("set_toolRootNodes", this);
+
+      this .set_toolRootNodes ();
+   }
+
+   set_toolRootNodes ()
+   {
       try
       {
-         const tool = this .node .getInnerNode () .addTool ();
+         this .#tool = this .node .getInnerNode () .addTool ();
 
-         tool .toolPointingEnabled = false;
+         this .#tool .toolPointingEnabled = false;
       }
       catch
-      { }
+      {
+         this .#tool = null;
+      }
    }
 
    disposeTool ()
    {
       try
       {
+         this .node .getBody () .rootNodes .removeInterest ("set_toolRootNodes", this);
+
          this .node .getInnerNode () .removeTool ();
       }
       catch
@@ -32,7 +45,7 @@ class X3DPrototypeInstanceTool extends X3DNodeTool
 
    getInnerNode ()
    {
-      return this .node .getInnerNode ();
+      return this .#tool;
    }
 }
 
