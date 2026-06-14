b00decea27433716ee61ac55aa2a839dcc32a508
Holger Seelig <holger.seelig@gmail.com>
2026-06-14 08:59:42 +0200

    Use installed version of electron.

--- build/publish-to-github.js
+++ build/publish-to-github.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 "use strict";
 
-const { systemSync } = require ("shell-tools");
+const { systemSync, sh } = require ("shell-tools");
 
 function main ()
 {
+   const electron = sh ("npm list electron") .match (/(electron@.*?)\n/s);
+
    systemSync (`git pull origin`);
    systemSync (`npm up`);
-   systemSync (`npm i -D electron`);
+   systemSync (`npm i -D ${electron}`);
    systemSync (`electron-forge publish`);
-   systemSync (`npm i -P electron`);
+   systemSync (`npm i -P ${electron}`);
 }
 
 main ();
