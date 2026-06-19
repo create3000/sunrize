608d905ef81d6a916180b9fe7b6a338d6d3266e6
Holger Seelig <holger.seelig@gmail.com>
2026-06-20 00:21:49 +0200

    Use X3DUOM 4.1.

--- build/X3DUOM.sh
+++ build/X3DUOM.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -q -O - https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml > src/assets/X3DUOM.xml
+wget -q -O - https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.1.xml > src/assets/X3DUOM.xml
