fa6b18ce5de0cabf945dd7b285f25ff2669d12f4
Holger Seelig <holger.seelig@gmail.com>
2026-07-13 22:29:01 +0200

    Fixed formatting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
+import js               from "@eslint/js";
+import globals          from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+export default defineConfig ([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals:
+      {
+        ... globals .browser,
+        ... globals .node,
+      }
+    }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions:
+    {
+      sourceType: "commonjs",
+    }
+  },
 ]);
