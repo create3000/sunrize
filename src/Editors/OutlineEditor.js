d62657a01b49e82d365f2b4f2c0939345464df29
Holger Seelig <holger.seelig@gmail.com>
2026-08-07 02:37:50 +0200

    Fix visibility condition for "Import Node..." in OutlineEditor.

--- src/Editors/OutlineEditor.js
+++ src/Editors/OutlineEditor.js
@@ -500,7 +500,7 @@ module .exports = class OutlineEditor extends OutlineRouteGraph
             },
             {
                label: _("Import Node..."),
-               visible: executionContext !== this .executionContext && !element .closest (".instance-scene, .InlineGeometry") .length,
+               visible: executionContext !== this .executionContext && !element .closest (".instance-scene, .InlineGeometry.internal-scene") .length,
                args: ["addImportedNode", element .attr ("id")],
             },
          ];
