4708ac2b43b748bb0224b03915bdb38de9a13ec8
Holger Seelig <holger.seelig@gmail.com>
2026-09-24 11:45:20 +0200

    Added canvas.

--- src/Editors/RoutingEditor.js
+++ src/Editors/RoutingEditor.js
@@ -3,9 +3,7 @@
 
 const
    $         = require ("jquery"),
-   electron  = require ("electron"),
    Interface = require ("../Application/Interface"),
-   util      = require ("util"),
    _         = require ("../Application/GetText");
 
 module .exports = class RoutingEditor extends Interface
@@ -21,11 +19,61 @@ module .exports = class RoutingEditor extends Interface
          .addClass (["toolbar", "vertical-toolbar", "secondary-toolbar", "routing-toolbar"])
          .appendTo (this .editor);
 
+      this .canvas = $("<canvas></canvas>") .addClass ("routes") .appendTo (this .left);
+
+      this .resizer = new ResizeObserver (() => this .resizeCanvas ());
+      this .resizer .observe (this .left [0]);
+
+      this .nodes = $("<div></div>") .addClass ("nodes") .appendTo (this .left);
+
       this .setup ();
    }
 
    configure ()
    {
       super .configure ();
+   }
+
+   colorScheme (/* shouldUseDarkColors */)
+   {
+      this .requestDrawRoutes ();
+   }
+
+   resizeCanvas ()
+   {
+      const
+         canvasWidth  = this .canvas .width (),
+         canvasHeight = this .canvas .height ();
+
+      this .canvas
+         .prop ("width",  canvasWidth)
+         .prop ("height", canvasHeight);
+
+      this .drawRoutes ();
+   }
+
+   #updateCanvasId = undefined;
+
+   requestDrawRoutes ()
+   {
+      clearTimeout (this .#updateCanvasId);
+
+      this .#updateCanvasId = setTimeout (() => this .drawRoutes ());
+   }
+
+   #style = window .getComputedStyle ($("body") [0]);
+
+   drawRoutes ()
+   {
+      const
+         context      = this .canvas [0] .getContext ("2d"),
+         canvasWidth  = this .canvas .width (),
+         canvasHeight = this .canvas .height ();
+
+      const background = this .#style .getPropertyValue ("--routes-background-color");
+
+      context .fillStyle = background;
+
+      context .fillRect (0, 0, canvasWidth, canvasHeight);
    }
 };
